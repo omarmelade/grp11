69d657bf3158ab2dded4c87329c020d6eb73ae86
o.khial <omarkhial74@gmail.com>
2020-02-18 14:07:48 +0100

    System.out "bonjour"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+        System.out.println("Bonsoir Thomas");
     }
 
     public static void main(String[] args){
