cbba05eb74e3f20c877bf325a2a94ead3645dcac
o.khial <omarkhial74@gmail.com>
2020-03-10 16:39:32 +0100

    add new interface with movable toolbarController  toolbar.fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("userView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 
         Scene scene = new Scene(root);
 
---
+++ src/sample/controller/MainController.java
@@ -0,0 +1,58 @@
+package sample.controller;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class MainController implements Initializable {
+
+    @FXML
+    private StackPane rootPane;
+    @FXML
+    private JFXHamburger hamburger;
+    @FXML
+    private JFXDrawer drawer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+            initDrawer();
+    }
+
+    private void initDrawer(){
+        try {
+            VBox toolbar = FXMLLoader.load(getClass().getResource("../toolbar.fxml"));
+            drawer.setSidePane(toolbar);
+            drawer.close();
+        }catch (IOException ignored){
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ignored);
+        }
+
+        HamburgerSlideCloseTransition task = new HamburgerSlideCloseTransition(hamburger);
+        task.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (Event event) -> {
+                task.setRate(task.getRate() * -1 );
+                task.play();
+                if(drawer.isClosed()){
+                    drawer.open();
+                }
+                else
+                {
+                    drawer.close();
+                }
+            });
+        }
+    }
