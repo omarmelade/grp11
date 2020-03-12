e384a986b0a1ec86c28deb6ecd477abcb0b3015c
Thomas Chin <thomas07.chin600@gmail.com>
2020-03-12 12:07:33 +0100

    Ajout de OnAction sur le bouton de connexion, cela change de page. (sans condition).

--- src/sample/controller/MainController.java
+++ src/sample/controller/MainController.java
@@ -1,16 +1,23 @@
 package sample.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,33 +34,50 @@ public class MainController implements Initializable {
     private JFXHamburger hamburger;
     @FXML
     private JFXDrawer drawer;
+    @FXML
+    private JFXButton loginButton;
+
+    private static void handle(MouseEvent event) {
+        System.out.println("test");
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-            initDrawer();
+        //initDrawer();
     }
 
-    private void initDrawer(){
+    private void initDrawer() {
         try {
             VBox toolbar = FXMLLoader.load(getClass().getResource("../view/toolbarController.fxml"));
             drawer.setSidePane(toolbar);
             drawer.close();
-        }catch (IOException ignored){
+        } catch (IOException ignored) {
             Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ignored);
         }
 
         HamburgerBackArrowBasicTransition task = new HamburgerBackArrowBasicTransition(hamburger);
         task.setRate(-1);
         hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (Event event) -> {
-                task.setRate(task.getRate() * -1 );
-                task.play();
-                if(drawer.isOpened()){
-                    drawer.close();
-                }
-                else
-                {
-                    drawer.open();
-                }
-            });
+            task.setRate(task.getRate() * -1);
+            task.play();
+            if (drawer.isOpened()) {
+                drawer.close();
+            } else {
+                drawer.open();
+            }
+        });
+    }
+
+    @FXML
+    private void loadConnect(ActionEvent event) {
+        try {
+            Parent blah = FXMLLoader.load(getClass().getResource("../home2.fxml"));
+            Scene scene = new Scene(blah);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.show();
+        } catch (IOException e) {
+            System.out.println("Une erreur s'est produite !");
         }
     }
+}
